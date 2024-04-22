6da29e3ad01702ec9d3e208b290ea3ec97c9d871
Wiktor Zieliński <120274586+wiktorze@users.noreply.github.com>
2024-04-22 16:07:24 +0200

    nic

--- archagnelsky_implementation.r
+++ archagnelsky_implementation.r
@@ -8,6 +8,7 @@ se = sqrt(vcov(tau.hat, method='placebo'))
 sprintf('point estimate: %1.2f', tau.hat)
 sprintf('95%% CI (%1.2f, %1.2f)', tau.hat - 1.96 * se, tau.hat + 1.96 * se)
 plot(tau.hat)
+
 # example
 setup = panel.matrices(california_prop99)
 tau.hat = synthdid_estimate(setup$Y, setup$N0, setup$T0)
