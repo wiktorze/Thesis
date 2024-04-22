3f7c0742c87e14cd433c74e3b0769e85648f6439
Wiktor Zieliński <120274586+wiktorze@users.noreply.github.com>
2024-04-22 16:02:23 +0200

    synthdidpackage

---
+++ archagnelsky_implementation.r
@@ -0,0 +1,17 @@
+rm(list = ls())
+pacman::p_load(devtools, data.table, synthdid)
+dt = fread("dt_treated_tract_17031080400.csv")
+dt = dt[,c("census_t_1", "time_index", "total", "treat_post")]
+setup = panel.matrices(dt)
+tau.hat = synthdid_estimate(setup$Y, setup$N0, setup$T0)
+se = sqrt(vcov(tau.hat, method='placebo'))
+sprintf('point estimate: %1.2f', tau.hat)
+sprintf('95%% CI (%1.2f, %1.2f)', tau.hat - 1.96 * se, tau.hat + 1.96 * se)
+plot(tau.hat)
+# example
+setup = panel.matrices(california_prop99)
+tau.hat = synthdid_estimate(setup$Y, setup$N0, setup$T0)
+se = sqrt(vcov(tau.hat, method='placebo'))
+sprintf('point estimate: %1.2f', tau.hat)
+sprintf('95%% CI (%1.2f, %1.2f)', tau.hat - 1.96 * se, tau.hat + 1.96 * se)
+plot(tau.hat)
